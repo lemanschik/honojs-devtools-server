8b8a9eb9d1b5541e0fa0a7664afc23db193dfd94
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-11-04 07:41:53 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,20 @@
-import { createServer as createServerHTTP } from 'node:http'
-import type { AddressInfo } from 'node:net'
 import { getRequestListener } from './listener'
 import type { Options, ServerType } from './types'
 
 export const createAdaptorServer = (options: Options): ServerType => {
   const fetchCallback = options.fetch
-  const requestListener = getRequestListener(fetchCallback, {
+  return getRequestListener(fetchCallback, {
     hostname: options.hostname,
     overrideGlobalObjects: options.overrideGlobalObjects,
     autoCleanupIncoming: options.autoCleanupIncoming,
   })
-  // ts will complain about createServerHTTP and createServerHTTP2 not being callable, which works just fine
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const createServer: any = options.createServer || createServerHTTP
-  const server: ServerType = createServer(options.serverOptions || {}, requestListener)
-  return server
+  
 }
 
 export const serve = (
   options: Options,
   listeningListener?: (info: AddressInfo) => void
 ): ServerType => {
-  const server = createAdaptorServer(options)
-  server.listen(options?.port ?? 3000, options.hostname, () => {
-    const serverInfo = server.address() as AddressInfo
-    listeningListener && listeningListener(serverInfo)
-  })
-  return server
+  return createAdaptorServer(options)
+
 }
